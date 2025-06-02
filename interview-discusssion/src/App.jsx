d0cab4926a646d454b35e731ba9c82013fe50cb0
Sanjoy Kumar Maity <sanjoykumarmaity05@gmail.com>
2025-06-02 20:27:25 +0530

    Interview question 2

--- interview-discusssion/src/App.jsx
+++ interview-discusssion/src/App.jsx
@@ -1,29 +1,37 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [value,setValue] = useState(1);
+  console.log("App rerendered", Math.random());
+  const [value, setValue] = useState({
+    value: 0,
+  });
+
   // const [multipliedValue,setMultipliedValue] = useState(1)
-  let multipliedValue = value * 5;
+  //let multipliedValue = value * 5;
 
-  const multiplyByFive = () =>{
-    // setMultipliedValue(value*5)
-    setValue(value + 1)
-  }
+  // const multiplyByFive = () =>{
+  // setMultipliedValue(value*5)
+  // setValue(value + 1)
+  // }
 
+  const clickMe = () => {
+    setValue({
+      value: 0,
+    });
+  };
 
+  useEffect(()=>{},[value.value])
 
   return (
     <>
-      <h1>Main value: {value}</h1>
-      <button
-      onClick={multiplyByFive}
-      >Click to multiply by 5</button>
-      <h1>Multiplied value: {multipliedValue}</h1>
+      <h1>Main value: {value.value}</h1>
+      <button onClick={clickMe}>Click to multiply by 5</button>
+      {/* <h1>Multiplied value: {multipliedValue}</h1> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
